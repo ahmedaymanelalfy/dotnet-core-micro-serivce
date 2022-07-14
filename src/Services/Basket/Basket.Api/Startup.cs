4461cb3c3b8c683e2d25ad86cb5d8e77aced1ed4
glow.solutions.eg <glow.solutions.eg@gmail.com>
2022-07-14 03:30:27 +0200

    create basket controller action and add repos

--- src/Services/Basket/Basket.Api/Startup.cs
+++ src/Services/Basket/Basket.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Basket.Api.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,13 @@ namespace Basket.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.Api", Version = "v1" });
             });
+            services.AddStackExchangeRedisCache(option =>
+            {
+                option.Configuration = Configuration.GetValue<String>("CasheSettings:ConnectionString");
+            
+            });
+
+            services.AddScoped<IBasketRepository, BasketRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
